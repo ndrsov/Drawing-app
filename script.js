c4494aab01d2e8048933c672c74e2af10bff7116
Andres Soria <andres.zov@gmail.com>
2022-09-09 14:07:12 -0500

    Create events to change brush size

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
+
 let isPressed = false;
-let size = 20;
-let color = "black";
+let size = 10;
+colorEl.value = "black";
+let color = colorEl.value;
 let x;
 let y;
 
@@ -43,3 +50,23 @@ function drawline(x1, y1, x2, y2) {
   ctx.lineWidth = size * 2;
   ctx.stroke();
 }
+
+function updateSizeOnScreen() {
+  sizeEl.innerText = size;
+}
+
+increaseBtn.addEventListener("click", () => {
+  size += 5;
+  if (size > 50) {
+    size = 50;
+  }
+  updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener("click", () => {
+  size -= 5;
+  if (size < 5) {
+    size = 5;
+  }
+  updateSizeOnScreen();
+});
